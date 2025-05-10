92b8182a9efb8f6e0445e66514f236bec7e011b7
mr-abdellah <belkaid.abdulah@gmail.com>
2025-05-10 14:10:38 +0100

    fixing migraion

--- database/migrations/2025_05_09_221931_create_orders_table.php
+++ database/migrations/2025_05_09_221931_create_orders_table.php
@@ -20,7 +20,7 @@ return new class extends Migration {
             $table->string('status')->default('pending');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
